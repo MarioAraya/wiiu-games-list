9955d176c287e2d2b675530e02a68e923fe200ce
MarioAraya <arayaromero@gmail.com>
2018-10-03 04:03:48 -0300

    closes the _blank page after open it

--- app.js
+++ app.js
@@ -6,8 +6,14 @@
   var html = template(juegos)
   document.getElementById("output").innerHTML = html
 
-  linkClick = function(el){
+  linkClick = function(el, url){
+    var wnd = window.open(url);
+    setTimeout(function() {
+      wnd.close();
+      document.execCommand("paste");
+    }, 3000);
     el.nextElementSibling.select()
     document.execCommand("copy");
+    return false;
   } 
 })();
